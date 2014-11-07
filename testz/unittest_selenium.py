df814074b4b0f3d5ad3aba85e3eea095ceb2ab5e
T3h-N1k0 <t3h_n1k0@lebib.org>
2014-11-07 11:20:45 +0100

    Selenium test added for search view

---
+++ testz/unittest_selenium.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+class LACSearchUser(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/")
+        elem_username = driver.find_element_by_name("username")
+        elem_pass  = driver.find_element_by_name("password")
+        elem_username.send_keys("chatelain_test")
+        elem_pass.send_keys("Sympalecines!")
+        driver.find_element_by_id("submit").click()
+
+
+    def test_search_by_uidNumber(self):
+        driver = self.driver
+        self.login()
+        driver.get("http://localhost:5000/search_user")
+        #self.assertIn("Recherche de compte", driver.title)
+        elem = driver.find_element_by_id("uid_number")
+        elem.send_keys("1053")
+        driver.find_element_by_id("submit").click()
+        assert u"Aucun résultat" not in driver.page_source
+
+    def test_search_by_sn(self):
+        driver = self.driver
+        self.login()
+        driver.get("http://localhost:5000/search_user")
+        #self.assertIn("Recherche de compte", driver.title)
+        elem = driver.find_element_by_id("sn")
+        elem.send_keys("chatelain")
+        driver.find_element_by_id("submit").click()
+        assert u"Aucun résultat" not in driver.page_source
+
+    def test_search_by_uid(self):
+        driver = self.driver
+        self.login()
+        driver.get("http://localhost:5000/search_user")
+        #self.assertIn("Recherche de compte", driver.title)
+        elem = driver.find_element_by_id("uid")
+        elem.send_keys("chatelain")
+        driver.find_element_by_id("submit").click()
+        assert u"Aucun résultat" not in driver.page_source
+
+    def test_search_by_mail(self):
+        driver = self.driver
+        self.login()
+        driver.get("http://localhost:5000/search_user")
+        #self.assertIn("Recherche de compte", driver.title)
+        elem = driver.find_element_by_id("mail")
+        elem.send_keys("chatelain@cines.fr")
+        driver.find_element_by_id("submit").click()
+        assert u"Aucun résultat" not in driver.page_source
+
+    def test_search_by_user_type(self):
+        driver = self.driver
+        self.login()
+        driver.get("http://localhost:5000/search_user")
+        #self.assertIn("Recherche de compte", driver.title)
+        elem = Select(driver.find_element_by_id("user_type"))
+        elem.select_by_visible_text("Compte CINES")
+        driver.find_element_by_id("submit").click()
+        assert u"Aucun résultat" not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_list_group_memberz_cines(self):
+        driver = self.driver
+        self.login()
+        driver.get("http://localhost:5000/")
+        driver.find_element_by_link_text("cines").click()
+        assert u"chatelain"  in driver.page_source
+
+
+
+if __name__ == "__main__":
+    unittest.main()
