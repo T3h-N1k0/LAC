3ab07306b7bf6f12fc9a79fb351992ea09eac114
T3h-N1k0 <t3h_n1k0@lebib.org>
2014-11-07 17:25:51 +0100

    Test for edit view added

--- testz/unittest_selenium.py
+++ testz/unittest_selenium.py
@@ -12,9 +12,9 @@ class LACSearchUser(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(4)
-        self.login()
+        self.admin_login()
 
-    def login(self):
+    def peon_login(self):
         driver = self.driver
         driver.get("{0}/".format(self.host))
         elem_username = driver.find_element_by_name("username")
@@ -79,20 +79,7 @@ class LACSearchUser(unittest.TestCase):
         driver.find_element_by_id("submit").click()
         assert u"Aucun résultat" not in driver.page_source
 
-    def test_list_group_memberz_cines(self):
-        driver = self.driver
-        driver.get("{0}/".format(self.host))
-        driver.find_element_by_link_text("cines").click()
-        assert u"chatelain"  in driver.page_source
-
-    def test_search(self):
-        self.search_by_uidNumber()
-        self.search_by_sn()
-        self.search_by_uid()
-        self.search_by_mail()
-        self.search_by_user_type()
-
-    def test_show_detailz(self):
+    def search_link_to_show(self):
         driver = self.driver
         driver.get("{0}/search_user".format(self.host))
         #self.assertIn("Recherche de compte", driver.title)
@@ -100,8 +87,70 @@ class LACSearchUser(unittest.TestCase):
         elem.send_keys("chatelain")
         driver.find_element_by_id("submit").click()
         driver.find_element_by_class_name('odd').click()
+
+    def own_account_view_peon(self):
+        driver = self.driver
+        driver.get("{0}/show/cines/chatelain".format(self.host))
         self.assertIn(u"Détails pour le compte chatelain", driver.title)
-        #assert u"" not in driver.page_source
+
+    def own_account_view_admin(self):
+        driver = self.driver
+        driver.get("{0}/show/cines/chatelain".format(self.host))
+        self.assertIn(u"Détails pour le compte chatelain", driver.title)
+
+    def get_edit_account_page(self):
+        driver = self.driver
+        driver.get("{0}/edit/cines/chatelain".format(self.host))
+        self.assertIn(u"Editer le compte chatelain", driver.title)
+
+    def test_list_group_memberz_cines(self):
+        driver = self.driver
+        driver.get("{0}/".format(self.host))
+        driver.find_element_by_link_text("cines").click()
+        assert u"chatelain"  in driver.page_source
+
+    def add_account_email(self):
+        driver = self.driver
+        driver.find_element_by_xpath(
+            "//div[@id='mail-fieldset']/button"
+        ).click()
+        new_mail = driver.find_element_by_id("mail-1")
+        new_mail.send_keys("test@test.test")
+        driver.find_element_by_id("update").click()
+
+    def check_added_account_email(self):
+        driver = self.driver
+        driver.get("{0}/show/cines/chatelain".format(self.host))
+        assert u"test@test.test"  in driver.page_source
+
+    def del_account_email(self):
+        driver = self.driver
+        driver.find_element_by_id('mail-0-remove').click()
+
+    def check_deleted_account_email(self):
+        driver = self.driver
+        driver.get("{0}/show/cines/chatelain".format(self.host))
+        assert u"test@test.test" not in driver.page_source
+
+
+    def test_search(self):
+        self.search_by_uidNumber()
+        self.search_by_sn()
+        self.search_by_uid()
+        self.search_by_mail()
+        self.search_by_user_type()
+        self.search_link_to_show()
+
+    def test_show_detailz(self):
+        self.own_account_view_admin()
+
+    def test_edit_account(self):
+        self.get_edit_account_page()
+        self.add_account_email()
+        self.check_added_account_email()
+        self.get_edit_account_page()
+        self.del_account_email()
+        self.check_deleted_account_email()
 
     def tearDown(self):
         self.driver.close()
