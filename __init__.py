78678d00126d310d602c1bff12b5d7ea5930fbeb
T3h-N1k0 <t3h_n1k0@lebib.org>
2015-08-06 17:29:14 +0200

    Reduce to 48H cookie expiration date

--- __init__.py
+++ __init__.py
@@ -4126,6 +4126,7 @@ def get_sambasid_prefix():
 if __name__ == '__main__':
     app.debug = app.config['DEBUG']
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(day=2)
     # toolbar = DebugToolbarExtension(app)
     decoder = PythonLDAPDecoder(app.config['ENCODING'])
     app.run(host='0.0.0.0')
