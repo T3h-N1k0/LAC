8c65e4d039cf75e7eba97f2556ddf9f1a10fc593
T3h-N1k0 <t3h_n1k0@lebib.org>
2015-10-02 11:30:33 +0200

    Cookie lifetime to 2 dayz

--- runserver.py
+++ runserver.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta, date
 from lac import app
 app.debug = app.config['DEBUG']
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-#app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(day=2)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(day=2)
 # toolbar = DebugToolbarExtension(app)
 #decoder = PythonLDAPDecoder(app.config['ENCODING'])
 app.run(host='0.0.0.0')
