d8b1e12260c3502e9657356d8bc360f986afdc57
T3h-N1k0 <t3h_n1k0@lebib.org>
2015-12-09 11:21:52 +0100

    Change dynamically Value when new unit chosen

--- lac/static/quota.js
+++ lac/static/quota.js
@@ -1,14 +1,12 @@
 $(function() {
-    $("table[class=quota-size]").each(function() {
+    $("table.quota-size").each(function() {
         var $this = $(this);
-        var old_unit;
-
-        $this.find("select").on('focus', function() {
+        $this.find("select").click(function() {
             old_unit = this.value;
         }).change(function() {
-            new_unit = this.value;
-            new_value = ($this.find("input").val() * old_unit) / new_unit;
-            $this.find("input").val(new_value)
+            var new_unit = this.value;
+            var new_value = ($this.find("input").val() * (old_unit / new_unit));
+            $this.find("input").val(new_value);
         });
     });
 });
