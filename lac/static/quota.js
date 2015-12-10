c08d4f871612a3fcba4564e86c9db76d5babffa9
T3h-N1k0 <t3h_n1k0@lebib.org>
2015-12-10 13:22:46 +0100

    Fixed bug on chrome on the select js

--- lac/static/quota.js
+++ lac/static/quota.js
@@ -1,11 +1,14 @@
 $(function() {
     $("table.quota-size").each(function() {
         var $this = $(this);
+        var old_unit =$this.find("select").val();
+        var new_unit;
+        var new_value;
         $this.find("select").click(function() {
             old_unit = this.value;
         }).change(function() {
-            var new_unit = this.value;
-            var new_value = ($this.find("input").val() * (old_unit / new_unit));
+            new_unit = this.value;
+            new_value = ($this.find("input").val() * (old_unit / new_unit));
             $this.find("input").val(new_value);
         });
     });
